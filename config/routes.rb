37c00a2e9ce14438e464cdb160023052c3059c86
maximedidry <maximedidry@gmail.com>
2023-04-15 18:27:02 +0200

    ✨ created first model + controller 🎨 did all html + added a navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'projects/index'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
 end
---
+++ db/migrate/20230415155125_add_index_to_projects_controller.rb
@@ -0,0 +1,4 @@
+class AddIndexToProjectsController < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
